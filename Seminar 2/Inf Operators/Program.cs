1f218704d27cd4133b0329e3851a2dc8e077c694
Suzi <s.ahajanyan@inbox.ru>
2024-06-23 01:57:07 +0400

    add Inf Operators

---
+++ Seminar 2/Inf Operators/Program.cs
@@ -0,0 +1,20 @@
+﻿// Унарный  Бинарний  Тернарный
+
+// У -7
+// Би 2 - 8
+// Тер 5 6 7
+
+//5 == 5 ? 6 : 7;
+
+
+// int number = 5; // = 3;
+// Console.WriteLine(number == 5 ? "мой ответ 5" : "мой ответ не 5");
+
+// int number = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine(number == 5 ? "мой ответ 5" : "мой ответ не 5");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+string output = number == 5 ? "положительный ответ" : "отрицательный ответ";
+
+Console.WriteLine(number == 5 ? "мой ответ 5" : "мой ответ не 5");
