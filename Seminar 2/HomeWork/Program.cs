89cdad15503fd8a26a2f74108e5b7a01de9efcf8
Suzi <s.ahajanyan@inbox.ru>
2024-06-29 20:39:39 +0400

    add HomeWork

--- Seminar 2/HomeWork/Program.cs
+++ Seminar 2/HomeWork/Program.cs
@@ -28,22 +28,50 @@
 // 78 -> третьей цифры нет
 // 32679 -> 6
 
-Console.WriteLine("Введите число: ");
+// Console.WriteLine("Введите число: ");
+// string str = Console.ReadLine();
+// int number = Convert.ToInt32(str);
+
+// if (number < 100)
+// {
+//     Console.WriteLine("Третьей цифры нет!");
+// }
+// else
+// {
+//     while (number >= 1000)
+//     {
+//         number = number /10;
+//     }
+//     int a = number % 10;
+//     Console.WriteLine("Третья цифра: " + a);
+
+// }
+
+
+
+
+
+// Напишите программу, 
+// которая принимает на вход цифру, 
+// обозначающую день недели, и проверяет, 
+// является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.WriteLine("Введите цифру, обозначающую день недели: ");
 string str = Console.ReadLine();
 int number = Convert.ToInt32(str);
 
-if (number < 100)
+if (number > 0 && number <= 5)
 {
-    Console.WriteLine("Третьей цифры нет!");
+    Console.WriteLine("рабочий день");
+}
+else if (number == 6 || number == 7)
+{
+    Console.WriteLine("Выходной");
 }
 else
 {
-    while (number >= 1000)
-    {
-        number = number /10;
-    }
-    int a = number % 10;
-    Console.WriteLine("Третья цифра: " + a);
-
+    Console.WriteLine("Это не день недели");
 }
-
