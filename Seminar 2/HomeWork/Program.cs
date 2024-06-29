b981e817d3e5ca05f2984b686c564780976aaf4b
Suzi <s.ahajanyan@inbox.ru>
2024-06-29 20:17:16 +0400

    add HomeWork

--- Seminar 2/HomeWork/Program.cs
+++ Seminar 2/HomeWork/Program.cs
@@ -5,16 +5,45 @@
 // // 782 -> 8
 // // 918 -> 1
 
-Console.WriteLine("Введите трехзначное число: ");
+// Console.WriteLine("Введите трехзначное число: ");
+// string str = Console.ReadLine();
+// int number = Convert.ToInt32(str);
+
+// if (number >= 100 && number <= 999)
+// {
+//     int a = (number / 10) % 10;
+//     Console.WriteLine("Вторая цифра: " + a);
+// }
+// else
+// {
+//     Console.WriteLine("Повторите попытку,число не трехзначно!");
+// }
+
+
+
+// Напишите программу, 
+// которая выводит третью цифру заданного числа 
+// или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.WriteLine("Введите число: ");
 string str = Console.ReadLine();
 int number = Convert.ToInt32(str);
 
-if (number >= 100 && number <= 999)
+if (number < 100)
 {
-    int a = (number / 10) % 10;
-    Console.WriteLine("Вторая цифра: " + a);
+    Console.WriteLine("Третьей цифры нет!");
 }
 else
 {
-    Console.WriteLine("Повторите попытку,число не трехзначно!");
+    while (number >= 1000)
+    {
+        number = number /10;
+    }
+    int a = number % 10;
+    Console.WriteLine("Третья цифра: " + a);
+
 }
+
